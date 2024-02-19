fc0d10a5b51b0d910d26e614b5daac0df8a6f4df
Moon Seongeun <mon706a@kakao.com>
2024-02-19 14:02:36 +0900

    DB 수정 내용입니다

--- miniproject_ye/foodrecipe.sql
+++ miniproject_ye/foodrecipe.sql
@@ -3,19 +3,17 @@ use FOODIE;
 
 drop table if exists MENU;
 create table MENU
-(ID			INT auto_increment not null primary key,
-TIME		INTEGER	null,
-CALORIE		INTEGER null,
-MENU_NAME	VARCHAR(50) not null,
-IMAGE BLOB not null,
-CATEGORY_ID INT not null);
+( 	ID			INTEGER 	not null primary KEY autoincrement,
+	MENU_NAME	VARCHAR(50) not null,
+	CATEGORY_ID INT 		not null,
+	CONSTRAINT MENU_CATEGORY_FK FOREIGN KEY (CATEGORY_ID) REFERENCES CATEGORY(ID));
 
 select *
 from menu;
 
 drop table if exists USER;
 create table USER
-(ID  INT auto_increment  not null primary key,
+(ID  INTEGER not null primary key autoincrement,
 PW	 VARCHAR(50)  not null,
 USER_NAME  CHAR(50) not null,
 EMAIL  VARCHAR(50) not null);
@@ -23,52 +21,34 @@ EMAIL  VARCHAR(50) not null);
 drop table if exists CATEGORY;
 
 create table CATEGORY
-(ID INT auto_increment not null primary key,
+(ID INTEGER not null primary key autoincrement,
 CATEGORY_NAME VARCHAR(50) not null);
 
 drop table if exists RECIPE;
 create table RECIPE
-(ID  INT auto_increment	not null primary key,
+(ID  INTEGER not null primary key autoincrement,
+ING_NAME VARCHAR(50) null,
+COOK_TOOL VARCHAR(50) null,
+TIME INTEGER	null,
+IMAGE BLOB 		null,
 TITLE  CHAR(50) not null,
 USER_ID INT not null,
-MENU_ID INT not null);
+MENU_ID INT not null,
+CONSTRAINT RECIPE_MENU_FK FOREIGN KEY (MENU_ID) REFERENCES MENU(ID),
+CONSTRAINT RECIPE_USER_FK FOREIGN KEY (USER_ID) REFERENCES "USER"(ID));
 
 drop table if exists COMMENT;
 create table COMMENT
-(ID  INT auto_increment	not null primary key,
+(ID  INTEGER not null primary key autoincrement,
 CONTEXT  TEXT not null,
 USER_ID int not null,
 RECIPE_ID int not null,
-MENU_ID int not null);
+CONSTRAINT COMMENT_USER_FK FOREIGN KEY (USER_ID) REFERENCES "USER"(ID),
+CONSTRAINT COMMENT_RECIPE_FK FOREIGN KEY (RECIPE_ID) REFERENCES RECIPE(ID));
 
 drop table if exists RECIPE_PROCESS;
 create table RECIPE_PROCESS
-(ID  INT auto_increment	not null primary key,
-IMAGE   BLOB not null,
+(ID  INTEGER not null primary key autoincrement,
 PROCESS  TEXT not null,
-USER_ID INT not null,
-MENU_ID INT not null);
-
-drop table if exists INGREDIENT;
-create table INGREDIENT
-(ID  INT auto_increment  not null primary key,
-ING_NAME  VARCHAR(50)  not null);
-
-drop table if exists COOK_TOOL;
-create table COOK_TOOL
-(ID  INT auto_increment  not null primary key,
-TOOL_NAME   VARCHAR(50) not null);
-
-drop table if exists RECIPE_INGREDIENT;
-create table RECIPE_INGREDIENT
-(ID INT  auto_increment	not null primary key,
-RECIPE_ID INT not null,
-INGREDIENT_ID INT not null);
-
-drop table if exists RECIPE_TOOL;
-create table RECIPE_TOOL
-(ID INT  auto_increment	not null primary key,
-RECIPE_ID INT not null,
-COOK_TOOL_ID INT not null);
-
-
+RECIPE_ID int not null,
+CONSTRAINT RECIPE_PROCESS_RECIPE_FK FOREIGN KEY (RECIPE_ID) REFERENCES RECIPE(ID));
